13bdcc3a3ae2ee329ff01b125ff479f3504eecbb
Mathias Ezequiel Latronico <mathiezelat@gmail.com>
2022-12-02 18:59:08 -0300

    :tada: add: reactive form and validators

---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css'],
+})
+export class FormComponent implements OnInit {
+  registerForm = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  get name() {
+    return this.registerForm.get('name');
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  submit() {
+    this.registerForm.markAllAsTouched();
+
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+
+      this.registerForm.reset();
+    }
+  }
+}
